6de28cd26ee7dd84a0d7df800db88cb6b0a73afb
Desmond <Desmond@192.168.1.114>
2015-02-01 09:08:18 -0800

    Commit 6

--- IMDB/src/Main.java
+++ IMDB/src/Main.java
@@ -6,8 +6,18 @@ public class Main {
 		String userData = reader.readFile("u.user");
 		String movieData = reader.readFile("u.item");
 		String ratingsData = reader.readFile("u.data");
-		
-		System.out.println(ratingsData);
+		MovieLens100kTranslator translator = new MovieLens100kTranslator();
+		String[] userDataArray = userData.split("\n");
+		User[] userArray = new User[userDataArray.length];
+		int dif = 0;
+		for (int i = 0; i < userArray.length - dif; i++) {
+			userArray[i] = translator.lineToUser(userDataArray[i + dif]);
+			if (userArray[i] == null) {
+				dif++;
+				i--;
+			}
+		}
+		System.out.println(userArray[0]);
 	}
 	
 }
